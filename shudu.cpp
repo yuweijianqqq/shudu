a7588e469c4803fc06f6c65401954785a5e362bd
yuweijianqqq <45254038+yuweijianqqq@users.noreply.github.com>
2018-12-14 19:44:29 +0800

    Update shudu.cpp

--- shudu.cpp
+++ shudu.cpp
@@ -8,83 +8,83 @@
 #include<algorithm>
 using namespace std;
 
-int n,m;
+int n, m;
 int d[9][9];
 void shengcheng();
 void output();
 bool check();
-void jie();
 int panduan(char *q);
 bool sign = false;
 ofstream Output;
+ifstream input;
 
 //Éú³ÉÊı¶À 
 void shengcheng(int n)
 {
-	m=0;
-	int s[9]={0,3,6,1,4,7,2,5,8};//ĞĞµÄÆ½ÒÆ 
-	for(int i=0;i<2;i++)
+	m = 0;
+	int s[9] = { 0,3,6,1,4,7,2,5,8 };//ĞĞµÄÆ½ÒÆ 
+	for (int i = 0; i < 2; i++)
 	{
-		if(i!=0) next_permutation(s+1,s+3);//23ĞĞ±ä»» 
-		for(int j=0;j<6;j++)
+		if (i != 0) next_permutation(s + 1, s + 3);//23ĞĞ±ä»» 
+		for (int j = 0; j < 6; j++)
 		{
-			if(j!=0) next_permutation(s+3,s+6);//456ĞĞ±ä»»ş
-			for(int k=0;k<6;k++)
+			if (j != 0) next_permutation(s + 3, s + 6);//456ĞĞ±ä»»?
+			for (int k = 0; k < 6; k++)
 			{
-				if(k!=0) next_permutation(s+6,s+9);//789ĞĞ±ä»»
-				int y[9]={7,1,2,3,4,5,6,8,9};//³õÊ¼ĞĞ 
-				for(int l=0;l<40320;l++)
+				if (k != 0) next_permutation(s + 6, s + 9);//789ĞĞ±ä»»
+				int y[9] = { 7,1,2,3,4,5,6,8,9 };//³õÊ¼ĞĞ 
+				for (int l = 0; l < 40320; l++)
 				{
-				    if(l!=0) next_permutation(y+1,y+9);//ĞĞÖ®¼ä»» 
-				    for(int b=0;b<9;b++)
-				    d[0][b]=y[b];
-				    for(int b=0;b<9;b++)
-				    {
-				    	for(int c=0;c<9;c++)
-				    	{
-						 int p=c+s[b];
-				    	 d[b][c]=d[0][p%9];
-						 if(c==8) Output<<d[b][c];
-				    	 else Output<<d[b][c]<<" ";
+					if (l != 0) next_permutation(y + 1, y + 9);//ĞĞÖ®¼ä»» 
+					for (int b = 0; b < 9; b++)
+						d[0][b] = y[b];
+					for (int b = 0; b < 9; b++)
+					{
+						for (int c = 0; c < 9; c++)
+						{
+							int p = c + s[b];
+							d[b][c] = d[0][p % 9];
+							if (c == 8) Output << d[b][c];
+							else Output << d[b][c] << " ";
 						}
-				    	 
+
 						Output << endl;
 					}
-				Output << endl;
-			    m++;
-				if (m >= n)
-				return ;
+					Output << endl;
+					m++;
+					if (m >= n)
+						return;
 				}
 			}
 		}
 	}
-	
+
 }
 
 //ÅĞ¶ÏÊä³öµÄÊÇ·ñÎªÊı×Ö 
 int panduan(char *q)
 {
 	int len = strlen(q);
-	int a; 
-		for (int i = 0; i < len; i++)
-	    {
-		if (q[i] < '0'||q[i] > '9')
-	      {
-	    	a=1;
-		  }
-	    }
-		if(a==1) return 1;
-		else return 2;	
+	int a=0;
+	for (int i = 0; i < len; i++)
+	{
+		if (q[i] < '0' || q[i] > '9')
+		{
+			a = 1;
+		}
+	}
+	if (a == 1) return 1;
+	else return 2;
 }
 
 void output()//Êä³ö 
 {
-	for (int i=0;i<9;i++)
+	for (int i = 0; i < 9; i++)
 	{
-		for (int j =0; j<9;j++)
+		for (int j = 0; j < 9; j++)
 		{
-			if (j==8) Output << d[i][j];
-			else Output << d[i][j]<<" ";
+			if (j == 8) Output << d[i][j];
+			else Output << d[i][j] << " ";
 		}
 		Output << endl;
 	}
@@ -93,54 +93,94 @@ void output()//Êä³ö
 
 bool check(int n)
 {
-    int hang=n/9; 
-	int lie=n%9;
-	for(int i=0;i<9;i++)
+	int hang = n / 9;
+	int lie = n % 9;
+	for (int i = 0; i < 9; i++)
 	{
-		if(d[hang][i]==d[hang][lie]&&i!=lie)  return false;//¼ì²éĞĞ 
+		if (d[hang][i] == d[hang][lie] && i != lie)  return false;//¼ì²éĞĞ 
 	}
-	for(int i=0;i<9;i++)
+	for (int i = 0; i < 9; i++)
 	{
-		if(d[i][lie]==d[hang][lie]&&i!=hang ) return false;//¼ì²éÁĞ
+		if (d[i][lie] == d[hang][lie] && i != hang) return false;//¼ì²éÁĞ
 	}
 	int x = n / 9 / 3 * 3;
-    int y = n % 9 / 3 * 3; 
-    for (int i = x; i < x + 3; i++)
+	int y = n % 9 / 3 * 3;
+	for (int i = x; i < x + 3; i++)
+	{
+		for (int j = y; j < y + 3; j++)
+		{
+			if (d[i][j] == d[hang][lie] && i != hang && j != lie) return false;//¼ì²éĞ¡¾Å¹¬¸ñ 
+		}
+	}
+	return true;
+}
+int DFS(int n)
+{
+    if (n>80)
     {
-        for (int j = y; j < y + 3; j++)
-        {
-            if (d[i][j]==d[hang][lie]&&i!=hang&&j!=lie) return false;//¼ì²éĞ¡¾Å¹¬¸ñ 
-        }
+        output();
+        return 0;
     }
-    return true;
-} 
+    if(d[n/9][n%9] == 0){
+		for(int i = 1; i <= 9; ++i){
+			d[n/9][n%9] = i;//¸³Öµ
+			if(check(n)){//¿ÉÒÔ·Å
+				DFS(n+1);//½øÈëÏÂÒ»²ã
+			}
+		}
+		d[n/9][n%9] = 0;//»ØËİ
+	}else{
+		DFS(n+1);
+	}
+
+}
 
 int main(int argc, char *argv[])
 {
 	ifstream input;
 	Output.open("sudoku.txt");
-	if (strcmp(argv[1],"-c") == 0)
+	if (strcmp(argv[1], "-c") == 0)
 	{
-	    
-	 int b=panduan(argv[2]);
-	 if(b==1)
-	 {
-	 	cout<<"ÊäÈëÊı×Ö"<<endl; 
-	 }
-	 if(b==2)
-	 {
-	  		int n = atoi(argv[2]);
-	  		if(n>0&&n<=1000000)
-	  		  {
-	  			shengcheng(n);
-			  }
-			  else
-			  {
-			  	cout<<"ÊäÈëÕıÈ·µÄÊı×Ö"<<endl; 
-			  }
-		
-     }
-	}
-}
 
+		int b = panduan(argv[2]);
+		if (b == 1)
+		{
+			cout << "ÊäÈëÊı×Ö" << endl;
+		}
+		if (b == 2)
+		{
+			int n = atoi(argv[2]);
+			if (n > 0 && n <= 1000000)
+			{
+				shengcheng(n);
+			}
+			else
+			{
+				cout << "ÊäÈëÕıÈ·µÄÊı×Ö" << endl;
+			}
+
+		}
+	}
+	else if( strcmp(argv[1],"-s") == 0 )
+	{
+		input.open(argv[2]);
+		int i = 0, j = 0;
+		while (!input.eof())//ÊäÈëÖÁÎÄ¼ş½áÎ²
+		{
+			input >> d[i][j];
+			j++;
+			if (j>8) {
+				j=0;
+				i++;
+			}
+			if (i>8) 
+			{
+				DFS(0);//½â¸ø¶¨µÄÊı¶À
+				i = 0; j =0;
+			}
+		}
+	}
+	else cout << "Wrong input!" << endl;
+	return 0;
+}
 
